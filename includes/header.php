0d2d29f958f26ae5efbf56a2d1685fb29671b59c
Wavxr <91374388+Wavxr@users.noreply.github.com>
2024-07-08 00:18:00 +0800

    Modify: Changed Font Style

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>StudyHub Nook | PHP</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/custom.css">
   </head>
   <body>
     <?php include('navbar.php'); ?>
